48c934d52f51c28c7ebd62d52b9430ba3686ea28
jerjes-test <jerjesm@hotmail.com>
2022-07-16 10:13:58 -0500

    Sin variables de ambiente

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,10 +1,12 @@
-const APIEXT = process.env.REACT_APP_API;
-const API_TOKEN=process.env.REACT_APP_API_TOKEN;
+// const APIEXT = process.env.REACT_APP_API;
+// const API_TOKEN=process.env.REACT_APP_API_TOKEN;
+const APIEXT="https://api.themoviedb.org/3";
+const API_TOKEN="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4N2U2NmQzMzZhOWE1ODYyYzkyODc3YzdlNDI1MGRhYSIsInN1YiI6IjYyYmY5MDIyYWY2ZTk0MDQ2YzcwODFhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-rMNhweMKfrJvaOsSQ8C48YCA7OwjychohwZ2ljJ_rk";
 
 
 export function get(path) {
-  // {console.log("9-El valor de API_TOKEN es."+API_TOKEN);}
-  // {console.log("El valor de la variable APIEXT es:"+APIEXT);}
+  {console.log("9-El valor de API_TOKEN es."+API_TOKEN);}
+  {console.log("El valor de la variable APIEXT es:"+APIEXT);}
     return fetch(APIEXT + path, {
       headers: {
         Authorization:
